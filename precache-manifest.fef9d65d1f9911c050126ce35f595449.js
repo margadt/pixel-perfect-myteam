49b424535a53209682bfbfd6f7a497c3bd5a7076
Margad T <spikkozor@gmail.com>
2020-02-19 13:03:59 +0200

    Updates

---
+++ precache-manifest.fef9d65d1f9911c050126ce35f595449.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46e993e666656fee0b0fe593470b14b",
+    "url": "/pixel-perfect-myteam/index.html"
+  },
+  {
+    "revision": "e944bde44ef225f54dd2",
+    "url": "/pixel-perfect-myteam/static/css/main.dc4ee488.chunk.css"
+  },
+  {
+    "revision": "d5d45f2f5071a0de4c1f",
+    "url": "/pixel-perfect-myteam/static/js/2.018704d5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/pixel-perfect-myteam/static/js/2.018704d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e944bde44ef225f54dd2",
+    "url": "/pixel-perfect-myteam/static/js/main.b5ecac10.chunk.js"
+  },
+  {
+    "revision": "32bbf0faa607a4838d8f",
+    "url": "/pixel-perfect-myteam/static/js/runtime-main.be6e5d6f.js"
+  },
+  {
+    "revision": "fddc5fa6efb91e49befb6ad849da9b0f",
+    "url": "/pixel-perfect-myteam/static/media/Livvic-Bold.fddc5fa6.ttf"
+  },
+  {
+    "revision": "fe84dbc6ce317e6216d959cf7baed69c",
+    "url": "/pixel-perfect-myteam/static/media/Livvic-SemiBold.fe84dbc6.ttf"
+  },
+  {
+    "revision": "d1d950e09ee94a2bbdd4627e65143cbc",
+    "url": "/pixel-perfect-myteam/static/media/avatar-aiysha.d1d950e0.jpg"
+  },
+  {
+    "revision": "60ff8909e4d598e25c144f0b47d988e3",
+    "url": "/pixel-perfect-myteam/static/media/avatar-arthur.60ff8909.jpg"
+  },
+  {
+    "revision": "f16826cacf2b1a460d2eab969d343d0a",
+    "url": "/pixel-perfect-myteam/static/media/avatar-kady.f16826ca.jpg"
+  },
+  {
+    "revision": "fe4f785a6475f2ea47465eb9a6fcda38",
+    "url": "/pixel-perfect-myteam/static/media/bg-pattern-home-4-about-3.fe4f785a.svg"
+  },
+  {
+    "revision": "458c9fcdfef1f53beea9819bd3a2b4e8",
+    "url": "/pixel-perfect-myteam/static/media/bg-pattern-home-5.458c9fcd.svg"
+  },
+  {
+    "revision": "efcf9a22216f31e99d42af206a8227a9",
+    "url": "/pixel-perfect-myteam/static/media/icon-quotes.efcf9a22.svg"
+  }
+]);
